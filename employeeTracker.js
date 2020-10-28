530c849e93b387a01f713b58eea392324656bab2
Jared Seefried <jared.seefried@yahoo.com>
2020-10-27 20:06:54 -0600

    Added inquirer prompt questions if the for add, view or update and whether it's for a department, role or employee.

--- employeeTracker.js
+++ employeeTracker.js
@@ -13,18 +13,8 @@ var connection = mysql.createConnection({
 connection.connect(function(error) {
     if (error) throw error;
     console.log("connected as id " + connection.threadId);
-    // afterConnection();
+    addViewUpdate();
 });
-
-function afterConnection() {
-    connection.query("{UPDATE}", function (error, result) {
-        if (error) throw error;
-        console.table(result);
-
-        // postBid();
-        // connection.end();
-    });
-}
 
 class Department {
     constructor(name){
@@ -48,3 +38,62 @@ class Employee {
         this.managerID = managerID
     }
 }
+
+function addViewUpdate (){
+    inquirer.prompt ([
+        {
+            type: "list",
+            message: "Would you like to Add, View or Update a record?",
+            name: "addViewUpdate",
+            choices: ["Add", "View", "Update"]
+        }
+        
+    ]).then(selection => {
+            switch (selection.addViewUpdate){
+                case "Add":
+                    addRecord();
+                    break;
+                case "View":
+                    viewRecord();
+                    break;
+                case "Update":
+                    updateRecord();
+                    
+            }
+        })
+}
+
+function addRecord (){
+    inquirer.prompt ([
+        {
+            type: "list",
+            message: "Would you like to Add a Department, Role or Employee?",
+            name: "addType",
+            choices: ["Department", "Role", "Employee"]
+        }
+    ])
+}
+
+function viewRecord (){
+    inquirer.prompt ([
+        {
+            type: "list",
+            message: "Would you like to View a Department, Role or Employee?",
+            name: "viewType",
+            choices: ["Department", "Role", "Employee"]
+        }
+    ])
+}
+
+function updateRecord (){
+    inquirer.prompt ([
+        {
+            type: "list",
+            message: "Would you like to Update a Department, Role or Employee?",
+            name: "updateType",
+            choices: ["Department", "Role", "Employee"]
+        }
+    ])
+}
+
+
