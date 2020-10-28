25c58745b90acf1f4a548d7efb4990e1e9e2d726
Jared Seefried <jared.seefried@yahoo.com>
2020-10-28 16:37:07 -0600

    Fixed schema to unclude INDEXES and added values

--- employeeDB.sql
+++ employeeDB.sql
@@ -24,3 +24,41 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department(name)
+VALUES
+    ("Management"),
+    ("Marketing"),
+    ("IT and Development"),
+    ("Legal"),
+    ("Finance"),
+    ("Logistics"),
+    ("Project Management"),
+    ("Human Resources"),
+    ("Operations"),
+    ("Customer Service"),
+    ("Sales"),
+    ("Product");
+
+INSERT INTO role(title, salary, department_id)
+VALUES  
+    ("Executive", 200, 1),
+    ("Manager", 100, 1),
+    ("Project Manager", 75, 7),
+    ("Business Analyst", 80, 9),
+    ("Sales Representative", 50, 11),
+    ("Customer Service Represenative", 40, 10),
+    ("Administrative Assistant", 100, 1),
+    ("Marketing Analyst", 60, 2),
+    ("Developer", 100, 3),
+    ("Product Manager", 90, 12),
+    ("Human Resources Manager", 75, 8),
+    ("Financial Analyst", 50, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  
+    ("Gary", "KillsSpiders", 9, 2),
+    ("Jared", "Seefried", 4, 1),
+    ("Will", "Ferrell", 1, 1 ),
+    ("Nora", "BoBora", 5, 3),
+    ("Taylor", "Analyst", 12, 2);
